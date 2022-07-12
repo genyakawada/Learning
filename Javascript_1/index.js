598c6469f0c33d4b50442926f05a753efbfbde5c
genyakawada <gen.kawada18@gmail.com>
2022-07-12 23:13:42 +0900

    javascriptの練習20220712

--- Javascript_1/index.js
+++ Javascript_1/index.js
@@ -1,4 +1,18 @@
 //　コメント
-console.log('Hello');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
 
-let 
+
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    console.log(input.value);
+    add()
+});
+
+function add(){
+    const li =document.createElement("li");
+    li.innerText=input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value="";
+};
