8df09cd8a519985414fd13906fe267db6997ccf6
genyakawada <gen.kawada18@gmail.com>
2022-07-17 12:50:59 +0900

    JSの追加＆faviconのBug(コメントアウト)

---
+++ Javascript_2/index.js
@@ -0,0 +1,14 @@
+
+//変数の宣言
+let unko ='Hello World';
+unko = 'Hello World2!!';
+console.log(unko);
+
+//定数の宣言
+const bigUnko ='Hello Japan';
+console.log(bigUnko);
+
+//定数に代入するとUncaught TypeError: Assignment to constant variable.エラーが出る
+//bigUnko = 'hello Tokyo';
+console.log(bigUnko);
+
